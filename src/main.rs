2f2c98d493c522e66f1502d05a502c1f40ad6fc6
lanceferrer <lintzferr@gmail.com>
2024-05-08 11:04:52 -0400

    add staged size tolerance check, minor refactor

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,17 @@ struct Args {
         short,
         long,
         default_value_t = String::from("100 MB"),
-        help = "Set size tolerance, default is 100 MB"
+        help = "Set individual file size tolerance, default is 100 MB"
     )]
-    threshold: String,
+    file_tolerance: String,
+
+    #[arg(
+        short,
+        long,
+        default_value_t = String::from("500 MB"),
+        help = "Set commit size tolerance, default is 500 MB"
+    )]
+    staged_tolerance: String,
 
     #[arg(help = "Optional commit hash to compare against")]
     commit_hash: Option<String>,
@@ -23,11 +31,14 @@ struct Args {
 fn main() -> Result<(), git2::Error> {
     let args = Args::parse();
 
-    let threshold =
-        parse_size(&args.threshold).map_err(|_| git2::Error::from_str("Invalid size"))?;
+    let file_tolerance =
+        parse_size(&args.file_tolerance).map_err(|_| git2::Error::from_str("Invalid size"))?;
 
-    let threshold =
-        u32::try_from(threshold).map_err(|_| git2::Error::from_str("Could not convert to u32"))?;
+    let staged_tolerance =
+        parse_size(&args.staged_tolerance).map_err(|_| git2::Error::from_str("Invalid size"))?;
+
+    let file_tolerance = u32::try_from(file_tolerance)
+        .map_err(|_| git2::Error::from_str("Could not convert to u32"))?;
 
     // let repo = Repository::open(".")?;
 
@@ -38,7 +49,7 @@ fn main() -> Result<(), git2::Error> {
     //     obj.id()
     // };
 
-    if check_files(threshold).is_err() {
+    if check_files(file_tolerance, staged_tolerance).is_err() {
         // exit code zero
         exit(1);
     } else {
@@ -52,7 +63,7 @@ struct LargeFile {
     size: u32,
 }
 
-fn check_files(threshold: u32) -> Result<(), git2::Error> {
+fn check_files(file_tolerance: u32, staged_tolerance: u64) -> Result<(), git2::Error> {
     let repo = Repository::open(".")?;
 
     // Get the index (staging area)
@@ -61,7 +72,7 @@ fn check_files(threshold: u32) -> Result<(), git2::Error> {
     // Iterate over index, collect large files
     let large_files: Vec<LargeFile> = index
         .iter()
-        .filter(|entry| entry.file_size > threshold)
+        .filter(|entry| entry.file_size > file_tolerance)
         .map(|entry| LargeFile {
             path: entry.path,
             size: entry.file_size,
@@ -71,7 +82,7 @@ fn check_files(threshold: u32) -> Result<(), git2::Error> {
     if !large_files.is_empty() {
         large_files.into_iter().for_each(|file| {
             let file_size = (file.size as f64 / 10000.0).round() / 100.0;
-            let threshold = (threshold as f64 / 10000.0).round() / 100.0;
+            let threshold = (file_tolerance as f64 / 10000.0).round() / 100.0;
             unsafe {
                 let file_path = CString::from_vec_unchecked(file.path)
                     .to_str()
@@ -79,7 +90,7 @@ fn check_files(threshold: u32) -> Result<(), git2::Error> {
                     .to_string();
 
                 println!(
-                    "File {} has size {}, which is greater than the threshold {}.",
+                    "File {} has size {} MB, which is greater than the tolerance {} MB.",
                     file_path, file_size, threshold
                 )
             }
@@ -88,7 +99,24 @@ fn check_files(threshold: u32) -> Result<(), git2::Error> {
         return Err(git2::Error::from_str("File size exceeds tolerance"));
     }
 
+    let staged_size = index
+        .iter()
+        .fold(0, |acc, entry| acc + (entry.file_size as u64));
+
+    if staged_size > staged_tolerance {
+        println!(
+            "Total staged size is {} MB, which is greater than the tolerance {} MB.",
+            bytes_to_mb(staged_size),
+            bytes_to_mb(staged_tolerance)
+        );
+        return Err(git2::Error::from_str("Total staged size exceeds tolerance"));
+    }
+
     Ok(())
+}
+
+fn bytes_to_mb(bytes: u64) -> f64 {
+    (bytes as f64 / 10000.0).round() / 100.0
 }
 
 fn run(oid: Oid, size: f64) -> Result<(), git2::Error> {
